179605fb82790fe72c9ac6bc66919c17efe45633
Andraitis Vilius <vilius.andraitis@mif.stud.vu.lt>
2021-07-05 21:59:55 +0300

    basic backend setup

--- index.js
+++ index.js
@@ -5,25 +5,12 @@ const app = express();
 const port = 9999;
 const apiToken = 'd6ec65a52238e8d2bdb0e22179e2327c';
 
-app.get('/', (req, res) => {
-    console.log('User arrived at homepage')
-    res.send('<h1>HOME PAGE!</h1>');
+app.post('/clickedArticle', (req, res) => {
+    console.log(req.body);
 })
 
-app.get('/search', async (req, res) => {
-    const { q } = req.query;
-    if (!q) {
-        console.log('Empty search query entered')
-        res.send("<h1>Nothing found if nothing searched.</h1>")
-    } else {
-    console.log(`User searched for: ${q}`)
-    res.send(`<h1>Search results for: ${q}</h1>`)
-    }
-})
-
-app.get('/list', (req, res) => {
-    console.log('Listing articles')
-    res.send('<h1>Listing articles</h1>')
+app.post('/userActions', (req, res) => {
+    console.log(req.body);
 })
 
 app.listen(port, function(err){
