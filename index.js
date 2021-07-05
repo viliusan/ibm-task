f21a346e1a9b64cf9ce08e4453f18040f18e6950
Andraitis Vilius <vilius.andraitis@mif.stud.vu.lt>
2021-07-05 22:42:49 +0300

    working backend

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded())
+
 const port = 9999;
 const apiToken = 'd6ec65a52238e8d2bdb0e22179e2327c';
 
@@ -11,6 +14,7 @@ app.post('/clickedArticle', (req, res) => {
 
 app.post('/userActions', (req, res) => {
     console.log(req.body);
+    return "";
 })
 
 app.listen(port, function(err){
